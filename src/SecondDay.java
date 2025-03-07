b18fcc6205cd5bdeda3e548f1d7e630badb02173
DELL <bibek.katuwal@gmail.com>
2025-03-07 21:08:58 +0545

    variable created stored int value and sum dispalyed,logical operator

---
+++ src/SecondDay.java
@@ -0,0 +1,12 @@
+public class SecondDay {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=true;
+        if(a && b){
+            System.out.println("Y");
+        }
+        else{
+            System.out.println("N");
+        }
+    }
+}
