9b8f5b51022719105a10221b826d174d1931f30e
DELL <bibek.katuwal@gmail.com>
2025-03-07 20:44:22 +0545

    variable created stored int value

---
+++ src/secondday.java
@@ -0,0 +1,10 @@
+public class secondday {
+    public static void main(String[] args) {
+        System.out.println("bibekkatuwal");
+        int num1=3;
+        int num2=4;
+        int num3=3;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
