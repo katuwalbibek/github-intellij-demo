9c6509189672a5df4eb92fccd51780a8eef1ff5e
DELL <bibek.katuwal@gmail.com>
2025-03-07 20:52:39 +0545

    variable created stored int value and sum dispalyed

--- src/secondday.java
+++ src/secondday.java
@@ -4,7 +4,7 @@ public class secondday {
         int num1=3;
         int num2=4;
         int num3=3;
-        int sum=num1+num2+num3;
-        System.out.println(sum);
+        int sum1=num1+num2+num3;
+        System.out.println(sum1);
     }
 }
